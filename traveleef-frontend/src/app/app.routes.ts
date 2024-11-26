20241e07ef86fe874ba14016da72e93205d6348c
Rayankz <kherouarayan@gmail.com>
2024-11-26 19:05:34 +0100

    Ajout du composant d'inscription et de connexion de manière plus développer. Il manque plusieurs ajustement et la possibilité d'utiliser des fonctions de connexion avec le backend pour pouvoir se connecter et s'inscrire pour rendre ces pages fonctionnels.

--- traveleef-frontend/src/app/app.routes.ts
+++ traveleef-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/se-connecter', pathMatch: 'full' },
+  { path: 'se-connecter', component: LoginScreenComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
