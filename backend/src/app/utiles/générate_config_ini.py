adaf1667800cc67899299e994ffbe9d220973703
Rayankz <kherouarayan@gmail.com>
2024-12-15 15:22:03 +0100

    Modification de sécurité, il faudra fournir la clé api manuellement pour la connexion à google flight

--- backend/src/app/utiles/générate_config_ini.py
+++ backend/src/app/utiles/générate_config_ini.py
@@ -15,7 +15,7 @@ def generate_config():
         'name': 'traveleef'
     }
     config['serpapi'] = {
-        'API_KEY': '090bef042216971af43307a560f6441c71c2fab3af2b314a3d0141e0bb750870',
+        'API_KEY': '???', #??? -> Fournir la clé api
         'BASE_URL': 'https://serpapi.com/search/',
     }
 
