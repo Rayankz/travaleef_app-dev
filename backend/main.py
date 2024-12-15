57bf1203086e592ed92aa5c59af0e97afe06e34e
Rayankz <kherouarayan@gmail.com>
2024-12-15 18:37:47 +0100

    Mise à jour de la page recherche -> connexion avec le back + du main.py (ajout d'un cors pour la sécurité)

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,24 @@
 import os
 from flask import Flask
+from flask_cors import CORS  # Import de Flask-CORS
 from src.app.utiles.database import ConfigApi
 from src.app.controlleurs.vol_controller import vol_bp
 from src.app.controlleurs.utilisateur_controller import utilisateur_bp
-import configparser
 
 def create_app():
     # Initialisation de l'application Flask
     app = Flask(__name__)
-
     app.config.from_object(ConfigApi)
 
+    # Configuration CORS : Autoriser les requêtes depuis Angular
+    CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
+
+    # Enregistrement des Blueprints
     app.register_blueprint(utilisateur_bp, url_prefix="/user")
     app.register_blueprint(vol_bp, url_prefix="/api")
 
     return app
+
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True, port=5000)
